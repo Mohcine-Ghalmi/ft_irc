30e6eaa5716c09d3ee893533109653731ee4eb31
Mohcine Ghalmi <mghalmi@e3r7p7.1337.ma>
2024-10-14 15:51:31 +0100

    Remove real name field (not required by project specifications)

--- Client.hpp
+++ Client.hpp
@@ -7,7 +7,6 @@ class Client
         int socket_fd;
         std::string nickname;
         std::string username;
-        std::string realname;
         bool is_auth;
         bool is_operator;
         std::string buffer;
@@ -15,7 +14,6 @@ class Client
         Client();
         std::string getNickName();
         std::string getUserName();
-        std::string getRealName();
         bool getAuthStatus();
         int send_message(std::string message);
         int join_channel(std::string channel_name);
@@ -42,11 +40,6 @@ std::string Client::getUserName()
 {
     return this->username;
 }
-std::string Client::getRealName()
-{
-    return this->realname;
-}
-
 
 bool Client::getAuthStatus()
 {
