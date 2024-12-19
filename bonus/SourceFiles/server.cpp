46dd5b512896629930c6aa47d7eb51e9ec283728
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-12-19 18:14:32 +0100

    Update : sizeof

--- bonus/SourceFiles/server.cpp
+++ bonus/SourceFiles/server.cpp
@@ -36,7 +36,7 @@ Server::Server(int argc, char **argv) {
     checkArgs(argc, argv);
 
     struct addrinfo hints, *res;
-    memset(&hints, 0, sizeof hints);
+    memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
